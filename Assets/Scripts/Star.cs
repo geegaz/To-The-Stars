816d68321578572b8c242a24beccec84836e1ae4
Geegaz <geegazrd@gmail.com>
2022-11-20 11:54:34 +0100

    Added player colors

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -6,17 +6,26 @@ using UnityEngine.Events;
 public class Star : MonoBehaviour
 {
     public int playerID = -1;
+    public bool insideZone = false;
 
     public UnityEvent OnConnect = new UnityEvent();
 
     public List<Star> connectedStars;
     public int connectedStarsMax = 2;
 
+    private SpriteRenderer render;
+
+    private void Awake() {
+        render = GetComponent<SpriteRenderer>();
+    }
+
     public void Connect(Star otherStar, int _playerID) {
         connectedStars.Add(otherStar);
         otherStar.connectedStars.Add(this);
         if (playerID < 0) {
             playerID = _playerID;
+            render.color = GameManager.GetPlayerColorFromID(playerID);
+            otherStar.render.color = render.color;
         }
         if (otherStar.playerID < 0) {
             otherStar.playerID = _playerID;
