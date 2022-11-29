b10c9c7f1662c8a37db91f1886d3f29508862bb0
Geegaz <geegazrd@gmail.com>
2022-11-29 14:24:20 +0100

    Broken screenshot

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
+    [Header("Screenshot")]
     [SerializeField] private string screenshotFolder = "Screenshots";
+    [SerializeField] private Camera screenshotCam;
+
+    [Header("Display")]
     [SerializeField] private Image endImage;
     [SerializeField] private RectTransform endScores;
 
@@ -18,25 +22,27 @@ public class EndMenu : MonoBehaviour
     private void OnEnable() {
         Debug.Log("Ended game");
         // When the menu gets enabled, means that the game is over
-        screenshot = ScreenCapture.CaptureScreenshotAsTexture();
-        SaveScreenshot();
-        // Create sprite
-        endImage.sprite = Sprite.Create(screenshot, new Rect(0.0f,0.0f,screenshot.width,screenshot.height), new Vector2(0.5f,0.5f));
+        StartCoroutine("SaveScreenshot");
+        
     }
 
-    private void SaveScreenshot() {
-        if (screenshot != null) {
-            // Create and save screenshot to disc
-            string folderPath = Application.dataPath+"/../"+screenshotFolder;
-            if (!System.IO.Directory.Exists(folderPath)) {
-                System.IO.Directory.CreateDirectory(folderPath);
-            }
-            string filePath = "/ToTheStars_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+".png";
-            System.IO.File.WriteAllBytesAsync(folderPath + filePath, screenshot.EncodeToPNG());
-            
-            #if UNITY_EDITOR
-            UnityEditor.AssetDatabase.Refresh();
-            #endif
+    IEnumerator SaveScreenshot() {
+        yield return new WaitForEndOfFrame();
+        screenshot = ScreenCapture.CaptureScreenshotAsTexture();
+        
+        // Create and save screenshot to disc
+        string folderPath = Application.dataPath+"/../"+screenshotFolder;
+        if (!System.IO.Directory.Exists(folderPath)) {
+            System.IO.Directory.CreateDirectory(folderPath);
         }
+        string filePath = "/ToTheStars_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+".png";
+        System.IO.File.WriteAllBytesAsync(folderPath + filePath, screenshot.EncodeToPNG());
+        
+        #if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+        #endif
+
+        // Create sprite
+        endImage.sprite = Sprite.Create(screenshot, new Rect(0.0f, 0.0f, screenshot.width, screenshot.height), new Vector2(0.5f,0.5f));
     }
 }
